01018b83e804f3ee0566e35712f7a682e33dfa8b
Daniel Lynch <lynch4360@gmail.com>
2025-07-17 00:54:12 +0100

    Added comments to some of the class files

--- Views/GamePage.xaml.cs
+++ Views/GamePage.xaml.cs
@@ -1,11 +1,13 @@
 using Astari25.ViewModels;
-using System.Timers;
+using System.Timers; // needed for Elapsed EventArgs
 
 namespace Astari25.Views;
 
+// main game screen
 public partial class GamePage : ContentPage
 {
     private readonly GamePageViewModel _viewModel;
+    // Game Loop that runs 60 times a second
     private readonly System.Timers.Timer _gameTimer;
 
 
@@ -13,22 +15,29 @@ public partial class GamePage : ContentPage
     {
         InitializeComponent();
 
+        // setup the View Model and binding for graphics
         _viewModel = new GamePageViewModel();
         BindingContext = _viewModel;
         GameCanvas.Drawable = _viewModel.GameDrawable;
 
         // Setup game loop with System.Timers.Timer 60 frames
         _gameTimer = new System.Timers.Timer(16);
+
+        // timer gets connected to the game loop function
         _gameTimer.Elapsed += OnGameLoop;
-        _gameTimer.Start();
+        _gameTimer.Start(); // start ticking the timer
     }
 
+    // This is running ever frame. 60 per sec
+    // It update the game logic and redwars the screen
     private void OnGameLoop(object sender, ElapsedEventArgs e)
     {
+
+        // all of the UI things that change need to run here. the main thread
         MainThread.BeginInvokeOnMainThread(() =>
         {
-            _viewModel.Update();
-            GameCanvas.Invalidate();
+            _viewModel.Update(); // moves the player
+            GameCanvas.Invalidate(); // makes the game reprint the Canvas
         });
     }
 }
