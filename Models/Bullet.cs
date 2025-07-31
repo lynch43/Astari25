a102e1708a382af7f738ebf240be39d5193bd61d
Daniel Lynch <lynch4360@gmail.com>
2025-07-31 21:18:18 +0100

    created a bullet class, gave it coords and update method

---
+++ Models/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Astari25.Models
+{
+    internal class Bullet
+    {
+
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Speed { get; set; } = 10;
+
+        public Bullet(float startX, float startY) {
+            X = startX;
+            Y = startY;
+        }
+
+        public void Update() {
+            Y -= Speed; // bullet go up
+        }
+    }
+}
