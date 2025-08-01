1920596990037938fcaa205a0d0cf5d34ecfad5a
Daniel Lynch <lynch4360@gmail.com>
2025-08-01 17:41:16 +0100

    add enemy class. make enemy object and give it coordinate and get it to move downwards

---
+++ Models/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Astari25.Models
+{
+    class Enemy
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Speed { get; set; } = 2;
+
+        public Enemy(float startX, float startY) {
+            X = startX;
+            Y = startY;
+        }
+
+        public void Update() {
+            // move enemy down game screen spaceinvaders style for now
+            Y -= Speed;
+        }
+    }
+}
