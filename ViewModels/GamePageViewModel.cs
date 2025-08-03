fe174718485f6803d1e13222d8c9f721754a0eb1
Daniel Lynch <lynch4360@gmail.com>
2025-08-03 06:30:59 +0100

    so many bugs. save point BASIC FUNCTIONALITY

--- ViewModels/GamePageViewModel.cs
+++ ViewModels/GamePageViewModel.cs
@@ -28,12 +28,28 @@ namespace Astari25.ViewModels
             //Player.X+=2;
 
             // updat ethe collection
-            foreach (var bullet in Bullets) {
+            foreach (var bullet in Bullets.ToList()) {
                 bullet.Update();
             }
 
-            foreach (var enemy in Enemies) {
+            foreach (var enemy in Enemies.ToList()) {
                 enemy.Update();
+            }
+
+            foreach (var bullet in Bullets.ToList()) {
+
+                foreach (var enemy in Enemies.ToList()) {
+                    float dx = bullet.X - enemy.X;
+                    float dy = bullet.Y - enemy.Y;
+                    float distance = (float)Math.Sqrt(dx * dx + dy * dy);
+
+                    if (distance < 15) // destrooy when they overlap
+                    {
+                        Enemies.Remove(enemy);
+                        Bullets.Remove(bullet);
+                    }
+                }
+            
             }
 
             // Spawns enemy randomly within the 60 frames so 4 seconds?
