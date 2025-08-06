bbcc22998c8310d7fea4ab0f7a77c25a6914bff3
Daniel Lynch <lynch4360@gmail.com>
2025-08-06 17:43:01 +0100

    Show score and lives and have them tracking ( Broken at the moment ) Change backgronnd to Black for entire screen and tet to white on GP.xaml

--- Models/Player.cs
+++ Models/Player.cs
@@ -1,18 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 
 namespace Astari25.Models
 {
-    public class Player
+    public class Player : INotifyPropertyChanged
     {
+        private int _lives = 3;
+        public int Lives
+        {
+            get => _lives;
+            set
+            {
+                if (_lives != value)
+                {
+                    _lives = value;
+                    OnPropertyChanged(nameof(Lives));
+                }
+            }
+        }
+
+        private int _score = 0;
+        public int Score
+        {
+            get => _score;
+            set
+            {
+                if (_score != value)
+                {
+                    _score = value;
+                    OnPropertyChanged(nameof(Score));
+                }
+            }
+        }
 
         public float X { get; set; } = 300;
-        public float Y { get; set; } = 300;
+        public float Y { get; set; } = 550;
         public float Radius { get; set; } = 20;
 
-        public int Lives { get; set; } = 3;
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string name) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
 }
