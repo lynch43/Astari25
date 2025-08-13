917bae8bf46c5718126426ef2f35f000e8ccaac4
Daniel Lynch <lynch4360@gmail.com>
2025-08-13 19:19:21 +0100

    Added base model class for KillConfirmed a notification to player that the score is being added

---
+++ Models/KillConfirmed.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Astari25.Models
+{
+    public class KillConfirmed
+    {
+        public float X { get; }
+        public float Y { get; }
+        public int hitPoint { get; }
+        public int framesLeft { get; private set; }
+        public int totalFrames { get; }
+
+        public KillConfirmed(float x, float y, int hitPoint, int framesLeft, int totalFrames)
+        {
+            X = x;
+            Y = y;
+            this.hitPoint = hitPoint;
+            this.framesLeft = framesLeft;
+            this.totalFrames = totalFrames;
+        } 
+    }
+}
