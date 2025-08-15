ed0c66c85f7e025b7069825ef73ec1f65b3c288f
Daniel Lynch <lynch4360@gmail.com>
2025-08-15 01:31:30 +0100

    add app settings class to models folder

---
+++ Models/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Astari25.Models
+{
+    internal class AppSettings
+    {
+    }
+}
