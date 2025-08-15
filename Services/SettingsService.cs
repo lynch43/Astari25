9589dc80cd89b990624c1410799d2ec269844698
Daniel Lynch <lynch4360@gmail.com>
2025-08-15 20:25:55 +0100

    added services folder. with one for settings to handle the json

---
+++ Services/SettingsService.cs
@@ -0,0 +1,12 @@
+﻿using Astari25.Models;
+using Microsoft.Maui.Storage;
+using System.Text.Json; 
+
+
+namespace Astari25.Services
+{
+    public static class SettingsService
+    {
+        
+    }
+}
