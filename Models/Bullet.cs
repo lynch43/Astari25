8c562a146edcbc76c9cb580762bf60038d574f27
Daniel Lynch <lynch4360@gmail.com>
2025-08-20 20:01:11 +0100

    Add comments and tidied Bullet.cs

--- Models/Bullet.cs
+++ Models/Bullet.cs
@@ -4,23 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// File used for
+// - this is a single bullet fired by the plater
+// - Store position and movement speed
+// - Update() advances the bullet upward every frame
 
 namespace Astari25.Models
 {
     public class Bullet
     {
 
+        // Centre of bullet. will be pixels
         public float X { get; set; }
         public float Y { get; set; }
-        public float Speed { get; set; } = 10;
 
+        // How many for every frame that it goes up
+        public float Speed { get; set; } = 10f;
+
+        // Spawn a bullet at a given starting position. player
         public Bullet(float startX, float startY) {
             X = startX;
             Y = startY;
         }
 
+
+        // Move the bullet for a frame,
+        // - the value make bullet go up 
         public void Update() {
-            Y -= Speed; // bullet go up
+            Y -= Speed;
         }
     }
 }
