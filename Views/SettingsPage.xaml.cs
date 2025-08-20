89451554687ca019509a7420294672cca45b3726
Daniel Lynch <lynch4360@gmail.com>
2025-08-20 20:39:36 +0100

    Finished off comments in the code behing of settings and start page

--- Views/SettingsPage.xaml.cs
+++ Views/SettingsPage.xaml.cs
@@ -1,11 +1,17 @@
 using Astari25.ViewModels;
 namespace Astari25.Views;
 
+// File used for:
+// - code behind for the settings creen
+// - Creates and attaches to the SettingsViewModel so the page can bind data
+// Just initializes the View and wires up the BindingContext
 public partial class SettingsPage : ContentPage
 {
 	public SettingsPage()
 	{
 		InitializeComponent();
+
+		// Binds the page to its view model so zaml controls are able to read and write
 		BindingContext = new SettingsViewModel();
 	}
 }
--- Views/StartPage.xaml.cs
+++ Views/StartPage.xaml.cs
@@ -1,18 +1,27 @@
 using System;
 namespace Astari25.Views;
 
+// File used for
+// - The start screen. landing page. 
+// - Two button Start game and Settings
+// - Navigation pushes the page onto the stack
 public partial class StartPage : ContentPage
 {
     public StartPage()
     {
+        // start up the Xaml
         InitializeComponent();
     }
 
+    // Start button, navigates to the game screen
+
     private async void OnStartGameClicked(object sender, EventArgs e)
     {
+        // Everything has to be in a NavigationPage to use the PushToAsync
         await Navigation.PushAsync(new GamePage());
     }
 
+    // Settings button, navigates to the settings screen
     private async void OnSettingsClicked(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new SettingsPage());
