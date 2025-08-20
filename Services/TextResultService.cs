7cf1ebf439990b1d8eaa1f36b5e2fe0eaebb2376
Daniel Lynch <lynch4360@gmail.com>
2025-08-20 21:14:47 +0100

    Add service to write one line per game. appending score to a .txt file

---
+++ Services/TextResultService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Astari25.Services
+{
+    public static class TextResultService
+    {
+    }
+}
