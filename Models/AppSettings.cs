d88f98d56c4339b81eda6527fd8b7f028a8f9254
Daniel Lynch <lynch4360@gmail.com>
2025-08-20 19:58:19 +0100

    Add comments to AppSettings.cs

--- Models/AppSettings.cs
+++ Models/AppSettings.cs
@@ -4,11 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// File used for: 
+// - Defines what app wide settings look like
+// - Anything that is aved or loaded
+// - Persistence is handled in the SettingsViewModel
+
 namespace Astari25.Models
 {
     public class AppSettings
     {
-        // easy, normal, hard
+        // Difficulty for the game is Easy Medium or hard
+        // Defaults to Normal. The settings page binds to this
+        // and writes to preferences
         public string Difficulty { get; set; } = "Normal";
         
     }
--- Models/Bullet.cs
+++ Models/Bullet.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Astari25.Models
 {
     public class Bullet
